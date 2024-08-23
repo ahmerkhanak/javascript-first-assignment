101a84339881b75e1f4348cffe97e68119b4db0a
Ahmer Khan <ahmerkhan1297@gmail.com>
2024-08-23 00:17:25 -0700

    Add files via upload

---
+++ ass2/alert.js
@@ -0,0 +1,25 @@
+alert ("Error! Please enter a valid password.");
+alert (`Welcome to Js land
+Happy coding!`);
+alert ("Welcome to JS land...");
+alert ("Hello... I can run JS through my web browser's console");
+
+var name = "Ahmer";
+alert(name)
+var age = 21;
+alert(age)
+var proffesion = "Certified Mobile Application Development";
+alert(proffesion);
+
+alert(`    PIZZA
+    PIZZ
+    PIZ
+    PI
+    P`);
+
+alert ("my email address is official@ahmer.com");
+
+alert ("I am trying to learn from the Book A smarter \nway to learn Javascript")
+
+var line ="--------------- ║ ︵ 〄 ︵ ║ ---------------";
+alert (line);
